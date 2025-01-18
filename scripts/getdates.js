db5ef431d3d4ab5a224edd5c2e08881db6485980
Franco Cardenas <francocardenas.fc5@gmail.com>
2025-01-18 18:07:59 -0300

    week02

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,10 +1,7 @@
 const currentYear = new Date().getFullYear();
 
-// Insertar el año actual en el elemento con id "copyright-year"
 document.getElementById('copyright-year').textContent = currentYear;
 
-// Obtener la fecha de última modificación del documento
 const lastModifiedDate = document.lastModified;
 
-// Insertar la fecha de última modificación en el elemento con id "last-modified"
 document.getElementById('last-modified').textContent = lastModifiedDate;
